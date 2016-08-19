a65244fa3d4b5a5441f5d7b5391cbec8147d3075
Jake Taemoon Jo <whxoans@gmail.com>
2016-08-19 17:03:16 +0900

    Initial Commit

---
+++ FlickrSlideAlbum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrSlideAlbum
+//
+//  Created by Tae-moonCho on 2016. 8. 19..
+//  Copyright © 2016년 HyperConnect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
