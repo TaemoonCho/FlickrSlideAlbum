a0204d8f0d91ce4bd6f147867b7853878c9fb3f6
Jake Taemoon Jo <whxoans@gmail.com>
2016-08-22 16:00:31 +0900

    Add spec to worker thread(FeedManager)

--- FlickrSlideAlbumTests/FeedManagerTests.swift
+++ FlickrSlideAlbumTests/FeedManagerTests.swift
@@ -5,9 +5,38 @@ class FeedManagerTests: QuickSpec {
     override func spec() {
         describe("Feed manager download test") {
             let manager = FeedManager.sharedInstance
-            it("Shoul get shared instance") {
-                expect(manager).notTo(beNil());
-                expect(manager.feeds.count).toEventuallyNot(beGreaterThan(3), timeout: 1000000, pollInterval: 1, description: "");
+            context("Create") {
+                it("Should get shared instance as singleton") {
+                    let anotherManager = FeedManager.sharedInstance
+                    expect(manager).notTo(beNil())
+                    expect(anotherManager).to(beIdenticalTo(manager))
+                }
+            }
+            context("Working") {
+                var isPrepared = false
+                var firstFeed: Feed? = nil
+                var secondFeed: Feed? = nil
+                it("Should work the worker thread") {
+                    expect(manager.isRunning).to(beFalse())
+                    expect(firstFeed).to(beNil())
+                    expect(secondFeed).to(beNil())
+                    expect(manager.feeds.count).to(equal(0))
+                    manager.runWithPreparedBlock({ (prepared) -> Void in
+                        isPrepared = prepared
+                        manager.getNextFeedWithBlock({ (isNew, aFeed) -> Void in
+                            firstFeed = aFeed
+                        })
+                        manager.getNextFeedWithBlock({ (isNew, aFeed) -> Void in
+                            secondFeed = aFeed
+                        })
+                    })
+
+                    expect(isPrepared).toEventually(beTrue(), timeout: 1000, pollInterval: 1, description: "")
+                    expect(firstFeed).toEventuallyNot(beNil(), timeout: 1500, pollInterval: 1, description: "")
+                    expect(secondFeed).toEventuallyNot(beNil(), timeout: 1500, pollInterval: 1, description: "")
+                    expect(firstFeed).notTo(beIdenticalTo(secondFeed))
+                    expect(manager.feeds.count).toEventually(beGreaterThan(10), timeout: 1000, pollInterval: 1, description: "")
+                }
             }
         }
     }
